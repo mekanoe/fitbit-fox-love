838a8821066baa4dced2d1e1b55ffce78e3762a2
Kata <git@kat.cafe>
2020-07-21 17:53:27 -0400

    feat: bottom stats

--- app/elements.ts
+++ app/elements.ts
@@ -1,3 +1,8 @@
 import document from "document";
 
 export const clockText = document.getElementById("clockText");
+
+export const statsHR = document.getElementById("statsHR");
+export const statsSteps = document.getElementById("statsSteps");
+export const statsDays = document.getElementById("statsDays");
+export const statsCals = document.getElementById("statsCals");
--- app/ui.ts
+++ app/ui.ts
@@ -1,1 +1,31 @@
-export const updateUI = (now: Date) => {};
+import { HeartRateSensor } from "heart-rate";
+import { today } from "user-activity";
+import { statsDays, statsHR, statsSteps, statsCals } from "./elements";
+
+export const updateUI = (now: Date) => {
+  updateDays(now);
+  updateHR();
+  updateSteppies();
+};
+
+const daysStart = 1594247400000; // start of fox time
+const dayInterval = 84e6;
+const updateDays = (now: Date) => {
+  const daysNow = now.getTime();
+  const daysSince = (daysNow - daysStart) / dayInterval;
+
+  statsDays.text = `${daysSince.toFixed(2)}`;
+};
+
+// @ts-ignore
+const hrSensor = new HeartRateSensor();
+hrSensor.start();
+
+const updateHR = () => {
+  statsHR.text = hrSensor.heartRate || "---";
+};
+
+const updateSteppies = () => {
+  statsSteps.text = `${today.adjusted.steps}`;
+  statsCals.text = `${today.adjusted.calories}`;
+};
